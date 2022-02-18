fb17d8e3f7182535606bb01dcf0116bc33d21cb9
José Antonio Perdiguero López <perdy@perdy.io>
2022-02-18 13:12:49 +0100

    :sparkles: Under construction page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,18 @@ import Link from 'next/link'
 import { FlamaIcon, GithubIcon } from '~/components/icons'
 import ThemeModeSwitcher from '~/components/ThemeModeSwitcher'
 
+function Logo() {
+  return (
+    <Link href="">
+      <a className="flex items-center justify-start space-x-2 text-brand-500">
+        <span className="sr-only">Flama home page</span>
+        <FlamaIcon className="h-6 w-6" />
+        <span className="text-3xl">Flama</span>
+      </a>
+    </Link>
+  )
+}
+
 function Nav() {
   const entries = [
     { href: '/docs', title: 'Docs' },
@@ -9,7 +21,7 @@ function Nav() {
   ]
 
   return (
-    <nav className="pr-16 font-medium text-zinc-600 dark:text-zinc-400">
+    <nav className="font-medium text-zinc-600 dark:text-zinc-400">
       <ul className="flex space-x-10">
         {entries.map((entry) => (
           <li key={entry.href}>
@@ -25,41 +37,37 @@ function Nav() {
   )
 }
 
-function Menu() {
+function Social() {
   return (
-    <div className="max-w-8xl mx-auto border-b border-brand-500 px-4 sm:px-8">
-      <div className="flex items-center justify-between py-6">
-        <div className="flex justify-start lg:w-1 lg:flex-1">
-          <Link href="">
-            <a className="flex items-center justify-start space-x-2 text-brand-500">
-              <span className="sr-only">Flama home page</span>
-              <FlamaIcon className="h-6 w-6" />
-              <span className="text-3xl">Flama</span>
-            </a>
-          </Link>
-        </div>
-        <div className="relative hidden items-center justify-between md:flex">
-          <Nav />
-          <div className="flex items-center space-x-10 border-l border-brand-500 pl-16">
-            <ThemeModeSwitcher className="h-6 w-6 text-zinc-400 hover:text-zinc-500 dark:text-zinc-600 dark:hover:text-zinc-300" />
-            <a
-              href="https://github.com/perdy/flama"
-              className="block text-zinc-400 hover:text-zinc-500 dark:text-zinc-600 dark:hover:text-zinc-300"
-            >
-              <span className="sr-only">Flama on GitHub</span>
-              <GithubIcon className="h-6 w-6" />
-            </a>
-          </div>
-        </div>
-      </div>
+    <div className="flex items-center space-x-10">
+      <ThemeModeSwitcher className="h-6 w-6 text-zinc-400 hover:text-zinc-500 dark:text-zinc-600 dark:hover:text-zinc-300" />
+      <a
+        href="https://github.com/perdy/flama"
+        className="block text-zinc-400 hover:text-zinc-500 dark:text-zinc-600 dark:hover:text-zinc-300"
+      >
+        <span className="sr-only">Flama on GitHub</span>
+        <GithubIcon className="h-6 w-6" />
+      </a>
     </div>
   )
 }
 
 export default function Header() {
   return (
-    <div className="relative">
-      <Menu />
-    </div>
+    <header className="max-w-8xl mx-auto border-b border-brand-500 px-4 sm:px-8">
+      <div className="flex items-center justify-between py-6">
+        <div className="flex justify-start lg:w-1 lg:flex-1">
+          <Logo />
+        </div>
+        <div className="relative hidden items-center justify-between md:flex">
+          <div className="pr-16">
+            <Nav />
+          </div>
+          <div className="border-l border-brand-500 pl-16">
+            <Social />
+          </div>
+        </div>
+      </div>
+    </header>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,40 @@
 import Header from '../components/Header'
+import { useRouter } from 'next/router'
 
-export default function Home() {
+function Construction() {
   return (
     <>
-      <div className='relative bg-zinc-100, dark:bg-zinc-800'>
+      <div className="relative min-h-screen overflow-hidden bg-zinc-100 dark:bg-zinc-800">
+        <main className="mx-auto mt-10 max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
+          <section className="text-center">
+            <h1>
+              <span className="text-4xl">🔥 = 👨‍💻 + 🤖</span>
+            </h1>
+          </section>
+        </main>
+      </div>
+    </>
+  )
+}
+
+export default function Home() {
+  const { query } = useRouter()
+
+  if (query.dev === undefined) return <Construction />
+
+  return (
+    <>
+      <main className="relative min-h-screen overflow-hidden bg-zinc-100 dark:bg-zinc-800">
         <Header />
 
-        <section className='text-center px-8 min-h-screen'></section>
-      </div>
+        <article className="mx-auto mt-10 max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
+          <section className="text-center">
+            <h1>
+              <span className="text-4xl">🔥 = 👨‍💻 + 🤖</span>
+            </h1>
+          </section>
+        </article>
+      </main>
     </>
   )
 }
