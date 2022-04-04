8d6a56f6de1dfc855a64f63fe855473bdef7209d
José Antonio Perdiguero López <perdy@perdy.io>
2022-04-04 14:29:35 +0200

    :sparkles: PWA

--- next.config.js
+++ next.config.js
@@ -1,18 +1,27 @@
 const { withContentlayer } = require('next-contentlayer')
+const withPWA = require('next-pwa')
 
 /** @type {import('next').NextConfig} */
-module.exports = withContentlayer()({
-  reactStrictMode: true,
-  eslint: {
-    dirs: ['src'],
-  },
-  async redirects() {
-    return [
-      {
-        source: '/docs',
-        destination: '/docs/getting-started/installation',
-        permanent: true,
-      },
-    ]
-  },
-})
+module.exports = withPWA(
+  withContentlayer()({
+    reactStrictMode: true,
+    eslint: {
+      dirs: ['src'],
+    },
+    async redirects() {
+      return [
+        {
+          source: '/docs',
+          destination: '/docs/getting-started/installation',
+          permanent: true,
+        },
+      ]
+    },
+    pwa: {
+      dest: 'public',
+      disable: process.env.NODE_ENV === 'development',
+      register: true,
+      sw: 'service-worker.js',
+    },
+  })
+)
