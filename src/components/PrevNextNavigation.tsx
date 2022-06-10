fc8a0a56e3ca2e3ccdd4af07ef2b7c690670e22c
José Antonio Perdiguero López <perdy@perdy.io>
2022-06-10 13:06:03 +0200

    :bug: Fixes prev and next navigation

---
+++ src/components/PrevNextNavigation.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import Link from '@/components/Link'
+import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/solid'
+
+interface PrevNextNavigationProps {
+  prevTitle: string
+  prevHref: string
+  nextTitle: string
+  nextHref: string
+}
+
+export function PrevNextNavigation({
+  prevTitle,
+  prevHref,
+  nextTitle,
+  nextHref,
+}: PrevNextNavigationProps) {
+  return (
+    <div
+      className={`mt-10 flex items-center ${
+        prevTitle && nextTitle ? 'justify-between' : ''
+      } ${prevTitle && !nextTitle ? 'justify-start' : ''} ${
+        !prevTitle && nextTitle ? 'justify-end' : ''
+      }`}
+    >
+      {prevTitle && (
+        <Link className="inline-flex h-full items-center pr-2" href={prevHref}>
+          <ChevronLeftIcon className="h-4 pr-2" />
+          <span>{prevTitle}</span>
+        </Link>
+      )}
+      {nextTitle && (
+        <Link className="inline-flex h-full items-center pl-2" href={nextHref}>
+          <span>{nextTitle}</span>
+          <ChevronRightIcon className="h-4 pl-2" />
+        </Link>
+      )}
+    </div>
+  )
+}
