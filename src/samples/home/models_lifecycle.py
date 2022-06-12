0bc88781b1f924a4d3779a8a7e67ec321a23f0a2
José Antonio Perdiguero López <perdy@perdy.io>
2022-06-12 13:00:36 +0200

    :sparkles: Update models lifecycle example

--- src/samples/home/models_lifecycle.py
+++ src/samples/home/models_lifecycle.py
@@ -3,7 +3,7 @@ from flama import Flama, ModelComponentBuilder
 
 with open("/path/to/model.flm", "rb") as f:
     component = ModelComponentBuilder.loads(f.read())
-    ModelType = type(component.model)  # Get the type to allow inject dependency
+    ModelType = component.get_model_type  # Get the type to allow inject dependency
 
 
 app = Flama(components=[component])
@@ -11,5 +11,17 @@ app = Flama(components=[component])
 
 @app.get("/")
 def model_view(model: ModelType, model_input: str):
+    """
+    tags:
+        - model
+    summary:
+        Model prediction.
+    description:
+        Interact with the model to generate a prediction based on
+        given input
+    responses:
+        200:
+            description: Model prediction.
+    """
     model_output = model.predict(model_input)
     return {"model_output": model_output}
