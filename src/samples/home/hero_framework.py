7c5dccae541fa4c856fa4292fbf2581ba4ff0796
migduroli <migduroli@gmail.com>
2022-10-26 17:58:15 +0200

    :recycle: Modify code blocks to be compliant with last CLI version

--- src/samples/home/hero_framework.py
+++ src/samples/home/hero_framework.py
@@ -1,9 +1,8 @@
-import uvicorn
-from flama import Flama
+import flama
 
-app = Flama()
+app = flama.Flama()
 
 app.models.add_model("/puppy/", "/path/to/puppy_model.flm", name="Puppy")
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    flama.run(flama_app=app, server_host="0.0.0.0", server_port=8080)
