4845f25699514530c161bdb5faaa744794a0f509
José Antonio Perdiguero López <perdy@perdy.io>
2023-10-23 18:04:40 +0200

    :bug: No need to use .html suffix in url

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const { withContentlayer } = require('next-contentlayer')
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  trailingSlash: true,
   images: {
     unoptimized: true,
   },
--- src/components/docs/Menu.tsx
+++ src/components/docs/Menu.tsx
@@ -78,7 +78,7 @@ export function DocsMenu() {
         order: docs.order,
         content: docs,
       })),
-    [allDocs],
+    [],
   )
   const toc = useMemo<TOC<IDocs>>(() => tableOfContent(links), [links])
 
